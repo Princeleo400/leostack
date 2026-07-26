2f489867a10cbb4fd630b000b4161082f87721e9
Prince Leo <ohagamichael@gmail.com>
2026-07-26 05:05:40 +0100

    feat: integrate vite-plugin-prerender to enable static site generation for core routes

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { PrerenderPlugin } from "vite-plugin-prerender";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    PrerenderPlugin({
+      staticDir: "dist",
+      routes: ["/", "/about", "/resume", "/portfolios", "/contact"],
+    }),
+  ],
+
   server: {
     port: 5181, // Set your desired port
     // strictPort: true, // Optional: Exit if port 3000 is unavailable
   },
-})
+});
